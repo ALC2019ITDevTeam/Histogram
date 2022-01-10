dd5ff1ea97b534a561d9834aba619919d9e4dca1
stargazer1846923 <kondorymansat@gmail.com>
2022-01-10 23:39:32 +0900

    CSV出力機能をクラス化した

--- Histogram/Histogram/SaveCsv.cs
+++ Histogram/Histogram/SaveCsv.cs
@@ -11,38 +11,51 @@ namespace Histogram
     public static class SaveCsv
     {
         /// <summary>
-        /// CSVファイルを生成する
+        /// CSVファイルを保存する
+        /// </summary>
+        /// <param name="listView">リストビュー</param>
+        /// <param name="fileName">保存するCSVファイルのファイル名</param>
+        /// <param name="encoding">保存するCSVファイルのエンコード</param>
+        /// <returns>例外メッセージ</returns>
+        public static (bool, string) SaveCsvFile(ListView listView, string fileName, Encoding encoding)
+        {
+            try
+            {
+                GenerateCsvFile(listView, fileName, encoding);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.ToString());
+            }
+            return (true, null);
+        }
+
+        /// <summary>
+        /// CSVファイルを出力する
         /// </summary>
         /// <param name="listView">リストビュー</param>
         /// <param name="fileName">保存するCSVファイルのファイル名</param>
         /// <param name="encoding">保存するCSVファイルのエンコード</param>
         public static void GenerateCsvFile(ListView listView, string fileName, Encoding encoding)
         {
-            try
+            using (StreamWriter streamWriter = new StreamWriter(fileName, false, encoding))
             {
-                using (StreamWriter streamWriter = new StreamWriter(fileName, false, encoding))
+                string listHeader = string.Empty;
+                for (int i = 0; i < listView.Columns.Count; i++)
                 {
-                    string listHeader = string.Empty;
-                    for (int i = 0; i < listView.Columns.Count; i++)
-                    {
-                        listHeader += listView.Columns[i].Text + ",";
-                    }
-                    streamWriter.WriteLine(listHeader.Remove(listHeader.Length - 1, 1));
-
-                    foreach (ListViewItem listViewItem in listView.Items)
-                    {
-                        string listitem = string.Empty;
-                        foreach (ListViewItem.ListViewSubItem subItem in listViewItem.SubItems)
-                        {
-                            listitem += subItem.Text + ",";
-                        }
-                        streamWriter.WriteLine(listitem.Remove(listitem.Length - 1, 1));
-                    }
+                    listHeader += listView.Columns[i].Text + ",";
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("ファイル保存時にエラーが発生しました。\n\n詳細：" + ex.ToString(), "ヒストグラム分析プログラム", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                streamWriter.WriteLine(listHeader.Remove(listHeader.Length - 1, 1));
+
+                foreach (ListViewItem listViewItem in listView.Items)
+                {
+                    string listitem = string.Empty;
+                    foreach (ListViewItem.ListViewSubItem subItem in listViewItem.SubItems)
+                    {
+                        listitem += subItem.Text + ",";
+                    }
+                    streamWriter.WriteLine(listitem.Remove(listitem.Length - 1, 1));
+                }
             }
             return;
         }
